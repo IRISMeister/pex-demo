0195526bd144066c964e86969de72618cbeae03f
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2021-11-08 17:03:07 +0900

    add .net framework 4.5 example

---
+++ dotnetfw45/PEX/PEXService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using InterSystems.Data.IRISClient.ADO;
+using InterSystems.Data.IRISClient.Gateway;
+using InterSystems.EnsLib.PEX;
+
+namespace test
+{
+    public class PEXService : BusinessService
+    {
+        /// <summary>
+        /// Configuration item(s) to which to send file stream messages
+        /// </summary>
+        public string TargetConfigNames;
+
+        /// <summary>
+        /// Configuration item(s) to which to send file stream messages, parsed into string[]
+        /// </summary>
+        private string[] targets;
+
+        /// <summary>
+        /// Connection to InterSystems IRIS
+        /// </summary>
+        private IRIS iris;
+
+        private int counter = 0;
+
+        public override void OnInit()
+        {
+            LOGINFO("Initialization started");
+
+            if (TargetConfigNames != null)
+            {
+                targets = TargetConfigNames.Split(',');
+            }
+            iris = GatewayContext.GetIRIS();
+
+            LOGINFO("Initialized!");
+        }
+
+        public override object OnProcessInput(object messageInput)
+        {
+            bool atEnd = false;
+            while (atEnd is false)
+            {
+                System.Threading.Thread.Sleep(1000);
+
+                foreach (string target in targets)
+                {
+                    IRISObject request = (IRISObject)iris.ClassMethodObject("Ens.StringContainer", "%New", "test message from .NET #"+ counter);
+                    SendRequestAsync(target, request);
+                }
+                counter++;
+            }
+            return null;
+        }
+
+        public override void OnTearDown()
+        {
+            iris.Close();
+        }
+    }
+}
---
+++ dotnetfw45/readme.txt
@@ -0,0 +1,4 @@
+Microsoft Visual Studio 2017にてビルド。
+
+PEXService.cs
+１秒間隔で、メッセージ(Ens.StringContainer)を送信するだけのビジネスサービス。
